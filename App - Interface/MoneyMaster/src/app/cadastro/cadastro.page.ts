10772c137f55007f9f3b396293bb0859b57c8d5c
Vitor <vitaocodes@gmail.com>
2023-04-20 14:19:33 -0300

    feat: Iniciando a página de 'Cadastro', importando módulos, adicionando componentes

--- App - Interface/MoneyMaster/src/app/cadastro/cadastro.page.ts
+++ App - Interface/MoneyMaster/src/app/cadastro/cadastro.page.ts
@@ -1,15 +1,18 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CadastroPage implements OnInit {
 
